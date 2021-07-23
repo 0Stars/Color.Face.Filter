34fda4bf9ba7e4c9a67f56742697419f47e11326
Acidic_Bloxy <79489448+Code-studio456@users.noreply.github.com>
2021-07-23 18:13:57 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,26 @@
-noseX=0;
-noseY=0;
-
 function preload() {
-  clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 }
 
 function setup() {
-  canvas = createCanvas(300, 300);
-  canvas.center();
+  canvas = createCanvas(640, 480);
+  canvas.position(375, 300);
   video = createCapture(VIDEO);
-  video.size(300, 300);
   video.hide();
 
-  poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose', gotPoses);
-}
-
-function modelLoaded() {
-  console.log('PoseNet Is Initialized');
-}
-
-function gotPoses(results)
-{
-  if(results.length > 0)
-  {
-    console.log(results);
-    noseX = results[0].pose.nose.x-15;
-    noseY = results[0].pose.nose.y-15;
-  }
+  tint_color = "";
 }
 
 function draw() {
-  image(video, 0, 0, 300, 300);
-  image(clown_nose, noseX, noseY, 30, 30);
+  image(video, 0, 0, 640, 480);
+  tint(tint_color);
 }
 
 function take_snapshot(){    
-  save('myFilterImage.png');
+  save('student_name.png');
 }
+
+function filter_tint()
+{
+	tint_color = document.getElementById("color_name").value;
+}
+
